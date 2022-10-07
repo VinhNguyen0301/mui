30b3c39c9d9b98256cab47ba626e1aae11d71f2d
VinhNguyen <vinh.nguyen@dedoco.com>
2022-10-07 14:25:59 +0700

    Add new UI responsive

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,22 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <Box
+      bgcolor="skyBlue"
+      flex={1}
+      p={2}
+      sx={{
+        display: {
+          xs: "none",
+          sm: "block",
+        },
+      }}
+    >
+      SideBar
+    </Box>
+  );
+};
+
+export default SideBar;
